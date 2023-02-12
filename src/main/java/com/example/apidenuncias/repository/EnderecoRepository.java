d5a611dec44d5b597c500ef967c69d26bba91cb7
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2023-02-12 17:45:00 -0300

    feat: create Enderecorepository

---
+++ src/main/java/com/example/apidenuncias/repository/EnderecoRepository.java
@@ -0,0 +1,7 @@
+package com.example.apidenuncias.repository;
+
+import com.example.apidenuncias.model.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
