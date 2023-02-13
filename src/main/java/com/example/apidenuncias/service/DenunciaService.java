34d2d5d4c9bc5283cb7be0feb313739681e166cd
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2023-02-12 22:12:14 -0300

    refactor: created saveDenuncia for save data in db and set json return with addres, user and complaint

--- src/main/java/com/example/apidenuncias/service/DenunciaService.java
+++ src/main/java/com/example/apidenuncias/service/DenunciaService.java
@@ -1,9 +1,16 @@
 package com.example.apidenuncias.service;
 
 import com.example.apidenuncias.model.Denuncia;
+import com.example.apidenuncias.model.Denunciante;
+import com.example.apidenuncias.model.Endereco;
 import com.example.apidenuncias.repository.DenunciaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.util.Map;
+
+@Service
 public class DenunciaService {
     @Autowired
     private DenunciaRepository denunciaRepository;
@@ -12,7 +19,20 @@ public class DenunciaService {
         this.denunciaRepository = denunciaRepository;
     }
 
-    public Denuncia save(Denuncia denuncia) {
-        return denunciaRepository.save(denuncia);
+    public Denuncia saveDenuncia(Map<String, Object> denunciaMap, Denunciante denunciante, Endereco endereco) {
+        Map<String, Object> denunciaDados = (Map<String, Object>) denunciaMap.get("denuncia");
+        Denuncia denuncia = new Denuncia();
+        denuncia.setTitulo((String) denunciaDados.get("titulo"));
+        denuncia.setDescricao((String.valueOf((String) denunciaDados.get("descricao"))));
+        denuncia.setDenunciante(denunciante);
+        denuncia.setEndereco(endereco);
+        denuncia.setCriacao(new Date());
+
+        denuncia = denunciaRepository.save(denuncia);
+
+        return denuncia;
+    }
+    public void save(Denuncia denuncia) {
+        denunciaRepository.save(denuncia);
     }
 }
