6d5141058cbbc762a8905a9ff999f2b57f319d58
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2023-02-12 22:10:08 -0300

    refactor: add notation Service and created validate return external api

--- src/main/java/com/example/apidenuncias/service/LocalizacaoService.java
+++ src/main/java/com/example/apidenuncias/service/LocalizacaoService.java
@@ -4,15 +4,19 @@ import com.example.apidenuncias.repository.EnderecoRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hibernate.service.spi.ServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Service
 public class LocalizacaoService {
+
     @Autowired
     private RestTemplate restTemplate;
 
@@ -24,19 +28,23 @@ public class LocalizacaoService {
         String url = "https://www.mapquestapi.com/geocoding/v1/reverse?key=nT5dm3pi2FTfmjEKpfR48CnETTSs3MMe"
                 + "&location=" + latitude + "," + longitude
                 + "&outFormat=json&thumbMaps=true";
-        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        try {
+            ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
 
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode root = mapper.readTree(response.getBody());
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode root = mapper.readTree(response.getBody());
 
-        Map<String, Object> resultJson = new HashMap<>();
-        resultJson.put("logradouro", root.path("results").get(0).path("locations").get(0).path("street").asText());
-        resultJson.put("bairro", root.path("results").get(0).path("locations").get(0).path("adminArea6").asText());
-        resultJson.put("cidade", root.path("results").get(0).path("locations").get(0).path("adminArea5").asText());
-        resultJson.put("estado", root.path("results").get(0).path("locations").get(0).path("adminArea3").asText());
-        resultJson.put("pais", root.path("results").get(0).path("locations").get(0).path("adminArea1").asText());
-        resultJson.put("cep", root.path("results").get(0).path("locations").get(0).path("postalCode").asText());
+            Map<String, Object> resultJson = new HashMap<>();
+            resultJson.put("logradouro", root.path("results").get(0).path("locations").get(0).path("street").asText());
+            resultJson.put("bairro", root.path("results").get(0).path("locations").get(0).path("adminArea6").asText());
+            resultJson.put("cidade", root.path("results").get(0).path("locations").get(0).path("adminArea5").asText());
+            resultJson.put("estado", root.path("results").get(0).path("locations").get(0).path("adminArea3").asText());
+            resultJson.put("pais", root.path("results").get(0).path("locations").get(0).path("adminArea1").asText());
+            resultJson.put("cep", root.path("results").get(0).path("locations").get(0).path("postalCode").asText());
 
-        return resultJson;
+            return resultJson;
+        } catch (Exception e) {
+            throw new ServiceException("Não foi possível obter a localização a partir das coordenadas informadas");
+        }
     }
 }
