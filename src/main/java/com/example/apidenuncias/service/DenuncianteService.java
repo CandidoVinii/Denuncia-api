56574140d230234396adf2eeb312429bf48aafcb
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2023-02-12 22:12:42 -0300

    refactor: created saveDenunciante for save new user to db

--- src/main/java/com/example/apidenuncias/service/DenuncianteService.java
+++ src/main/java/com/example/apidenuncias/service/DenuncianteService.java
@@ -3,13 +3,27 @@ package com.example.apidenuncias.service;
 import com.example.apidenuncias.model.Denunciante;
 import com.example.apidenuncias.repository.DenuncianteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
+@Service
 public class DenuncianteService {
     @Autowired
     private DenuncianteRepository denuncianteRepository;
 
     public DenuncianteService(DenuncianteRepository denuncianteRepository) {
         this.denuncianteRepository = denuncianteRepository;
+    }
+
+    public Denunciante saveDenunciante(Map<String, Object> denunciaMap) {
+        Map<String, Object> denuncianteMap = (Map<String, Object>) denunciaMap.get("denunciante");
+        Denunciante denunciante = new Denunciante();
+        denunciante.setNome((String) denuncianteMap.get("nome"));
+        denunciante.setCpf((String) denuncianteMap.get("cpf"));
+
+        denunciante = denuncianteRepository.save(denunciante);
+        return denunciante;
     }
 
     public Denunciante save(Denunciante denunciante) {
